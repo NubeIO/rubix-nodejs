43dfa8040c4b614669a40991ccc8a6aeaa5969f7
zero88 <sontt246@gmail.com>
2020-12-07 19:00:38 +0700

    Add script to install nodejs and pm2

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# rubix-nodejs
+# Rubix Nodejs
+
 Rubix Nodejs utilities
+
+- Install nodejs `v10` (default) or another version
+- Install `pm2` as `soft` global module by making `PATH` in current `node_modules`
+- Update `etc/rsyslog.conf` to allow:
+  - Redirect `pm2 logs` to `syslog`
+  - Enable `syslog` color
+  
+## HOWTO
+
+- Install nodejs v10: `./install.sh`
+- Install nodejs v12: `./install.sh 12`
+- Install nodejs v14: `./install.sh 14`
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+## Install nodejs
+nodeVersion="${1:-10}"
+echo "$nodeVersion"
+curl -sL "https://deb.nodesource.com/setup_$nodeVersion.x" | sudo -E bash -
+apt install node -y
+
+## Install PM2
+npm i
+echo "export PATH=\$PATH:$(pwd)/node_modules/.bin" >> ~/.profile
+source ~/.profile
+
+## Update rsyslog.conf
+sudo tee -a /etc/rsyslog.conf > /dev/null <<EOT
+
+#
+# ANSI color
+#
+\$EscapeControlCharactersOnReceive off
+
+#
+# provides UDP syslog reception
+#
+module(load="imudp")
+input(type="imudp" port="514")
+EOT
+
+sudo systemctl restart rsyslog
+
+## Install pm2-syslog
+pm2 install pm2-syslog
